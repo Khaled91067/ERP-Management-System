0457b4e5734cca0699826d05b8b5a1a76c4ae1c7
Kh91067 <khaledahmedwhyd1234@gmail.com>
2026-07-08 21:01:03 +0300

    chore: finalize persistence configuration

--- ERP.Application/Abstractions/Repositories/IgenericRepository.cs
+++ ERP.Application/Abstractions/Repositories/IgenericRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ERP.Application.Abstractions.Repositories
 {
-    public interface IgenericRepository<T> where T : class
+    public interface IGenericRepository<T> where T : class
     {
         Task<T?> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync(QueryOptions<T>? options=null);
