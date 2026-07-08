85d8b2c5770633d633db228718d74f6c8d459d9c
Kh91067 <khaledahmedwhyd1234@gmail.com>
2026-07-08 20:57:06 +0300

    feat: implement generic repository

---
+++ ERP.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using ERP.Application.Abstractions.Repositories;
+using ERP.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ERP.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(QueryOptions<T>? options = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if(options != null)
+            {
+                if(options.Filter != null)
+                {
+                    query = query.Where(options.Filter);
+
+                }
+               
+                foreach (var include in options.Includes)
+                {
+                    query = query.Include(include);
+                }
+
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ ERP.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using ERP.Application.Abstractions;
+using ERP.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
